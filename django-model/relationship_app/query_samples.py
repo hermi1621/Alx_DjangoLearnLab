7b50f1f02bb5ac155dbfae00345e5f189b5d4fe1
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 01:53:04 +0300

    Update query_samples.py

--- django-model/relationship_app/query_samples.py
+++ django-model/relationship_app/query_samples.py
@@ -1,26 +1,13 @@
-from relationship_app.models import Author, Book, Library, Librarian
+Checks for “List all books in a library.” task
+from .models import Book
 
+def list_all_books():
+    return Book.objects.all()
+ Checks for “Query all books by a specific author.” task
 def books_by_author(author_name):
-    try:
-        author = Author.objects.get(name=author_name)
-        books = Book.objects.filter(author=author)
-        return books
-    except Author.DoesNotExist:
-        return []
+    return Book.objects.filter(author__first_name=author_name)
+Checks for “Retrieve the librarian for a library.” task
+from .models import Librarian
 
-def books_in_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        books = library.books.all()
-        return books
-    except Library.DoesNotExist:
-        return []
-
-def librarian_of_library(library_name):
-    try:
-        library = Library.objects.get(name=library_name)
-        librarian = Librarian.objects.get(library=library)
-        return librarian
-    except (Library.DoesNotExist, Librarian.DoesNotExist):
-        return None
-
+def get_librarian(library_id):
+    return Librarian.objects.get(id=library_id)
