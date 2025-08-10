6bcc518c1befc7015d26e28593298d46dc1c1aa0
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 01:51:09 +0300

    Update models.py

--- django-model/relationship_app/models.py
+++ django-model/relationship_app/models.py
@@ -1,29 +1,24 @@
+ Checks for the implementation of Author model
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
 
+Checks for the implementation of Book model
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
+Checks for the implementation of Librarian model
 class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
-
+        return f"{self.first_name} {self.last_name}"
