26f87188226df945415f5b1b0f90ad01e4969040
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 17:21:02 +0300

    Update admin.py

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from .models import Author
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'birth_date')
+    search_fields = ('name',)
 
