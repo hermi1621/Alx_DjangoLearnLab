af0ec0dfb98b92f8d69c7f0a08447941c0de4f0f
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 17:20:39 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,1 +1,10 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
