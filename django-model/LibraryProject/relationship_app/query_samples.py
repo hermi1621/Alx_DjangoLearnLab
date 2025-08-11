f70a352c293d1dbd8fd980a54bfacb914f6f8aeb
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 18:37:12 +0300

    Update query_samples.py

--- django-model/LibraryProject/relationship_app/query_samples.py
+++ django-model/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,7 @@
-from .models import Author, Book, Library
+Author.objects.get(name=author_name)
+from .models import Author, Book
 
-# Query all books by a specific author
 def query_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)  # exact required line
+    author = Author.objects.get(name=author_name)
     books = Book.objects.filter(author=author)
     return books
-
-# Retrieve the librarian for a library
-def get_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    librarian = library.librarian  # assumes OneToOneField relationship
-    return librarian
