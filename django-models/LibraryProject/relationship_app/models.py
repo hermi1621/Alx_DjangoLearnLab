7aa9ce3b98fe08b2f176f2f4480ff21cbd88045f
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 17:39:09 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
+# Author model
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-
+# Book model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
@@ -15,10 +15,18 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+# Library model
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
 
+    def __str__(self):
+        return self.name
+
+# Librarian model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.CharField(max_length=200)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
