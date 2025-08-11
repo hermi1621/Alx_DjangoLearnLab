9adc42f6687e07428f37cfc72a12689b6e5bdb5b
hermi1621 <hermelabezabih4@gmail.com>
2025-08-11 18:40:41 +0300

    Update query_samples.py

--- django-model/LibraryProject/relationship_app/query_samples.py
+++ django-model/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,6 @@
-Author.objects.get(name=author_name)
 from .models import Author, Book
 
 def query_books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
+    author = Author.objects.get(name=author_name)  # must be exactly this line
     books = Book.objects.filter(author=author)
     return books
