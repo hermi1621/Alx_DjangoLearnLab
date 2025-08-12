803e01e250ff5b85c703db0dcdc5fe45712e7860
hermi1621 <hermelabezabih4@gmail.com>
2025-08-13 01:15:17 +0300

    Create settings.py

---
+++ advanced-api-project/advanced_api_project/settings.py
@@ -0,0 +1,17 @@
+INSTALLED_APPS = [
+    # Default Django apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Your apps
+    'api',
+]
+
+# Database: default SQLite is fine (no changes needed if default)
