a5061b10cef22e89fa3efbcbefb817cdc4ebb5a3
hermi1621 <hermelabezabih4@gmail.com>
2025-08-13 01:01:22 +0300

    Update urls.py

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -13,3 +13,10 @@ urlpatterns = [
     path('api/', include('api.urls')),  # Include the api app URLs here
 ]
 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the api app URLs here
+]
