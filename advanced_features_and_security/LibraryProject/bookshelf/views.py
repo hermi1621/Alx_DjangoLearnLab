3a00567387429f7ee8018d491e6bb6cd96fe3591
hermi1621 <hermelabezabih4@gmail.com>
2025-08-12 18:22:56 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import SearchForm
 
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+def book_search(request):
+    form = SearchForm(request.GET or None)
+    books = Book.objects.none()
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        # Safe ORM filter with parameterization
+        books = Book.objects.filter(title__icontains=query)
+    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
