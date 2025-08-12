f346beee48a0b536f1b671b83172ff9c23704cb2
hermi1621 <hermelabezabih4@gmail.com>
2025-08-13 01:00:00 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -4,3 +4,9 @@ from .views import BookListCreateAPIView
 urlpatterns = [
     path('api/books/', BookListCreateAPIView.as_view(), name='book_list_create'),
 ]
+from django.urls import path
+from .views import BookList
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+]
