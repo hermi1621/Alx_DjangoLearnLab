56edb03eb1e435048867cf537bd68be2d7e184f6
hermi1621 <hermelabezabih4@gmail.com>
2025-08-12 18:25:49 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from .models import Book
-from .forms import SearchForm
+from .forms import ExampleForm
 
-def book_search(request):
-    form = SearchForm(request.GET or None)
-    books = Book.objects.none()
-    if form.is_valid():
-        query = form.cleaned_data['query']
-        # Safe ORM filter with parameterization
-        books = Book.objects.filter(title__icontains=query)
-    return render(request, 'bookshelf/book_list.html', {'form': form, 'books': books})
+# your views here
