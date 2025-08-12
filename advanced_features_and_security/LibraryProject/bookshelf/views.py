f10a9547f39e3dc33c5f6dcb77242c5cbd10698b
hermi1621 <hermelabezabih4@gmail.com>
2025-08-12 18:16:00 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,44 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required, login_required
+from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 from .models import Book
-from .forms import BookForm  # if you have a form; replace with your form or simple logic
 
-# View for reading/listing (requires can_view)
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    return render(request, 'bookshelf/book_detail.html', {'book': book})
-
-# Create view (requires can_create)
-@permission_required('bookshelf.can_create', raise_exception=True)
-def create_book(request):
-    if request.method == 'POST':
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('books:list')  # change to your URL name
-    else:
-        form = BookForm()
-    return render(request, 'bookshelf/book_form.html', {'form': form})
-
-# Edit view (requires can_edit)
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
-        if form.is_valid():
-            form.save()
-            return redirect('books:detail', pk=book.pk)
-    else:
-        form = BookForm(instance=book)
-    return render(request, 'bookshelf/book_form.html', {'form': form, 'book': book})
-
-# Delete view (requires can_delete)
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_book(request, pk):
-    book = get_object_or_404(Book, pk=pk)
-    if request.method == 'POST':
-        book.delete()
-        return redirect('books:list')
-    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
