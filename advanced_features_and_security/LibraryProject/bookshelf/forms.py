0c72dba3100fce4449e2b924158244f0d8f49717
hermi1621 <hermelabezabih4@gmail.com>
2025-08-12 18:24:47 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
-class SearchForm(forms.Form):
-    query = forms.CharField(max_length=100)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
