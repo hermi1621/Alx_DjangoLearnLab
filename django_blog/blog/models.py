c7e100456acc7e0b779b2551c5d14ea2e2694085
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 09:17:17 +0300

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -31,3 +31,24 @@ class Comment(models.Model):
         return f"Comment by {self.author.username} on {self.post.title}"
 
 
+
+
+
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager  # for tagging
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()  # many-to-many relationship with tags
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
