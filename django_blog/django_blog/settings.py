b9b7b77d3b327b10caba93b0615adcfeef7fae7c
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 08:31:00 +0300

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -1,1 +1,11 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'blog',  # ✅ Added blog app
+]
 
