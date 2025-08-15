d466508781a51f8b0789bb0f98a01a6d2d35b245
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 09:24:07 +0300

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -9,3 +9,16 @@ DATABASES = {
     }
 }
 
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'blog',         # your blog app
+    'taggit',       # <-- add this line for tagging functionality
+]
