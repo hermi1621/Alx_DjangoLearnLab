96a41a74828d7a2705abad956511fda7a0715d42
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 07:42:44 +0300

    Update settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -3,3 +3,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'api',
 ]
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',  # Django REST Framework
+    'api',  # Our API app
+]
+
