46a4fc39b4c21f565fc5f6be104d70a6db8ec4bf
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 08:42:36 +0300

    Update urls.py

--- django_blog/blog/blog/urls.py
+++ django_blog/blog/blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 from .views import CustomLoginView, CustomLogoutView
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
