7a7bb3af50614ef4035f2d9f1987be7b5838647c
hermi1621 <hermelabezabih4@gmail.com>
2025-08-15 08:44:49 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from . import views
+from .views import CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),  # Registration URL
+    path('login/', CustomLoginView.as_view(), name='login'),  # Login URL
+    path('logout/', CustomLogoutView.as_view(), name='logout'),  # Logout URL
+    path('profile/', views.profile_view, name='profile'),  # Profile URL
+]
 
