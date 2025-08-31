912fd989277790641257e3aeeca2e71d4305be8e
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 16:35:54 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -31,3 +31,29 @@ def login(request):
 def profile(request):
     user = request.user
     return Response(UserSerializer(user).data)
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    request.user.followers.add(target_user)
+    return Response({'message': f'You are now following {target_user.username}'})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    target_user = get_object_or_404(User, id=user_id)
+    request.user.followers.remove(target_user)
+    return Response({'message': f'You have unfollowed {target_user.username}'})
+
+
