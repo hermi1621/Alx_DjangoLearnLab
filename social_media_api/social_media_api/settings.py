d0b5efa57715f24096cd5fbf482a5dae36538af3
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 16:12:36 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -1,1 +1,16 @@
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    # Local apps
+    'accounts',
+]
