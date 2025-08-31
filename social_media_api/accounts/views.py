bbca975f0a33696d00ffc28a5067697add54e970
hermi1621 <hermelabezabih4@gmail.com>
2025-08-31 17:02:09 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -55,3 +55,27 @@ class UnfollowUserView(generics.GenericAPIView):
         target_user = get_object_or_404(CustomUser.objects.all(), id=user_id)
         request.user.followers.remove(target_user)
         return Response({'message': f'You have unfollowed {target_user.username}'}, status=status.HTTP_200_OK)
+
+
+
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
+
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.followers.add(target_user)
+        return Response({'message': f'You are now following {target_user.username}'}, status=status.HTTP_200_OK)
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        target_user = get_object_or_404(CustomUser.objects.all(), id=user_id)
+        request.user.followers.remove(target_user)
+        return Response({'message': f'You have unfollowed {target_user.username}'}, status=status.HTTP_200_OK)
